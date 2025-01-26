57fbeef5cb9f7c01cf13ad7eaa42b466709e2bdf
Rishit <rishitbutola10@gmail.com>
2025-01-26 23:52:48 +0530

    arr

---
+++ arr.js
@@ -0,0 +1,9 @@
+//using constructors
+let array = new Array(5); // Creates an array with 5 empty slots
+let numbers = new Array(1, 2, 3, 4, 5); // Creates an array with initial values
+//using arr literals
+let arr = [1, 2, 3, 4, 5]; // Array of numbers
+let fruits = ["apple", "banana", "cherry"]; // Array of strings
+let mixed = [1, "hello", true, null]; // Array of mixed data types
+//initializing empty array
+let emptyArray = [];//empty Array
