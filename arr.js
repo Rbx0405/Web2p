e5b49000ff07d86af17acd7986836459f7b5191f
Rishit <rishitbutola10@gmail.com>
2025-01-27 00:06:39 +0530

    dom

--- arr.js
+++ arr.js
@@ -4,6 +4,10 @@ let numbers = new Array(1, 2, 3, 4, 5); // Creates an array with initial values
 //using arr literals
 let arr = [1, 2, 3, 4, 5]; // Array of numbers
 let fruits = ["apple", "banana", "cherry"]; // Array of strings
+//console.log shows result of value 0 ,1 and 2 in the given array 
+console.log(fruits[0]); // "apple"
+console.log(fruits[1]); // "banana"
+
 let mixed = [1, "hello", true, null]; // Array of mixed data types
 //initializing empty array
 let emptyArray = [];//empty Array
